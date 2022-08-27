42d653049ae236b46e58e5505c8be3a6420a4031
oreste <muhirwa.g.oreste@gmail.com>
2022-08-27 12:34:19 +0200

    update of the class, use inheritance, to get access to all match data , plus is selected, but this class should be deleted later, as it doent make sense, it is there now just for help the design

--- lib/model/mensdiv1comp.dart
+++ lib/model/mensdiv1comp.dart
@@ -1,54 +1,14 @@
-class Div1Matches {
-  String imageHome;
-  String imageAway;
-  String nameHome;
-  String nameAway;
-  String goalsHome;
-  String goalsAway;
+import 'package:frvb/model/match.dart';
+import 'package:frvb/model/team.dart';
+
+class Div1Matches extends Match {
   bool isSelected;
 
-  Div1Matches({
-    this.imageHome = '',
-    this.imageAway = '',
-    this.nameHome = '',
-    this.nameAway = '',
-    this.goalsHome = '',
-    this.goalsAway = '',
-    this.isSelected = false,
-  });
+  Div1Matches(super.homeTeam, super.awayTeam, this.isSelected);
 }
 
 List<Div1Matches> div1games = [
-  Div1Matches(
-      imageHome: "assets/clubs/rayon.webp",
-      nameHome: "Rayon",
-      imageAway: "assets/clubs/aprvc.png",
-      nameAway: "APR VC",
-      goalsAway: "3",
-      goalsHome: "3",
-      isSelected: true),
-  Div1Matches(
-      imageHome: "assets/clubs/gsob.png",
-      nameHome: "GSOB",
-      imageAway: "assets/clubs/aprvc.png",
-      nameAway: "APRVC",
-      goalsAway: "0",
-      goalsHome: "3",
-      isSelected: false),
-  Div1Matches(
-      imageHome: "assets/clubs/reg.webp",
-      nameHome: "REG VC",
-      imageAway: "assets/clubs/kirehe.png",
-      nameAway: "KIREHE",
-      goalsAway: "3",
-      goalsHome: "2",
-      isSelected: false),
-  Div1Matches(
-      imageHome: "assets/clubs/kvc.png",
-      nameHome: "KVC",
-      imageAway: "assets/clubs/gisagara.webp",
-      nameAway: "Gisagara",
-      goalsAway: "1",
-      goalsHome: "1",
-      isSelected: false),
+  Div1Matches(gsob, aprvc, false),
+  Div1Matches(reg, kirehe, false),
+  Div1Matches(kvc, gisagara, false)
 ];
