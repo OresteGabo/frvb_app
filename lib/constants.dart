4c003c556274b8c5e0ee35cfc021380596eea4e5
oreste <muhirwa.g.oreste@gmail.com>
2022-08-28 20:14:00 +0200

    Gray color added for icons

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const backgroundColor = Color.fromARGB(235, 246, 246, 246);
 const pinkColor = Color(0xffff0e80);
 const selectedColor = Color(0xffe5a457);
+const iconColors = Colors.grey;
