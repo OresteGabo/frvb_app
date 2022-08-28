b3a430e3a9c3b839a3c90f53bc1b287c0b7aa0fe
oreste <muhirwa.g.oreste@gmail.com>
2022-08-28 20:13:18 +0200

    CupertinoTabScaffold replaced with normal  Scafford, and a CupertinoTabBar is replaced with a normal BottomNavigationBar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -24,7 +24,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
+    /*return CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
           currentIndex: _currentIndex,
           onTap: onTabTapped,
@@ -60,7 +60,72 @@ class _MainPageState extends State<MainPage> {
               return _children[_currentIndex];
             },
           );
+        });*/
+    return Scaffold(
+      bottomNavigationBar: bottomNavigationBar(),
+      body: _children[_currentIndex],
+    );
+  }
+
+  NavigationBar navigationBar() {
+    return NavigationBar(
+      selectedIndex: _currentIndex,
+      backgroundColor: backgroundColor,
+      destinations: const [
+        NavigationDestination(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home'),
+        NavigationDestination(
+            icon: Icon(Icons.calendar_month), label: 'Calendar'),
+        NavigationDestination(
+            icon: Icon(Icons.bar_chart_rounded), label: 'Statistics'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Account'),
+      ],
+      onDestinationSelected: (index) {
+        setState(() {
+          _currentIndex = index;
         });
+      },
+    );
+  }
+
+  BottomNavigationBar bottomNavigationBar() {
+    return BottomNavigationBar(
+      //backgroundColor: Colors.green,
+
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home_rounded,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_month,
+            ),
+            label: 'calendar'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bar_chart_rounded,
+            ),
+            label: 'statist'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'account'),
+      ],
+      currentIndex: _currentIndex,
+      onTap: onTabTapped,
+      showUnselectedLabels: false,
+      showSelectedLabels: false,
+      selectedItemColor: Colors.amber[800],
+      unselectedItemColor: iconColors,
+      iconSize: 27,
+    );
   }
 
   void onTabTapped(int index) {
