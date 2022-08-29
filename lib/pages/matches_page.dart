c9c2096540a635669628ed31df2de9d7a957acc1
oreste <muhirwa.g.oreste@gmail.com>
2022-08-29 04:53:53 +0200

    implementation of a sliding_up_panel on a MatchCard, that will display more information on a selected match

--- lib/pages/matches_page.dart
+++ lib/pages/matches_page.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frvb/model/competition.dart';
 import 'package:frvb/model/match.dart';
-import 'package:frvb/pages/home_page.dart';
-import 'package:frvb/constants.dart';
 import 'package:badges/badges.dart';
 import 'package:frvb/pages/main_page.dart';
-import '../widgets/competitionWidget.dart';
-import '../widgets/match_card.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:frvb/widgets/competitionWidget.dart';
+import 'package:frvb/widgets/match_card.dart';
 
 class MatchesPage extends StatefulWidget {
-  const MatchesPage({Key? key}) : super(key: key);
+  const MatchesPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MatchesPage> createState() => _MatchesPageState();
@@ -20,9 +20,12 @@ class _MatchesPageState extends State<MatchesPage> {
   String _selectedCompetition = competitions[0].name;
   Match _selectedMatch = matchCards[0].match;
   bool _slideupPanelUp = false;
+  final panelController = PanelController();
 
   @override
   Widget build(BuildContext context) {
+    final panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
+    final panelHeightOpened = MediaQuery.of(context).size.height * 0.6;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -69,55 +72,134 @@ class _MatchesPageState extends State<MatchesPage> {
         ],
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 14, top: 32, right: 14),
-          child: Column(
-            children: [
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: competitions
-                      .map(
-                        (e) => InkWell(
-                            onTap: () => setState(() {
-                                  _selectedCompetition = e.name;
-                                }),
-                            child: CompetitionWidget(
-                                image: e.images,
-                                name: e.name,
-                                isSelected: e.name == _selectedCompetition)),
-                      )
-                      .toList(),
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: matchCards.map(
-                      (e) {
-                        return InkWell(
-                            onTap: () => setState(() {
-                                  _selectedMatch = e.match;
-                                  _slideupPanelUp = !_slideupPanelUp;
-                                }),
-                            child: MatchCard(
-                                match: e.match,
-                                isSelected: e.match == _selectedMatch));
-                      },
-                    ).toList(),
+        child: SlidingUpPanel(
+          controller: panelController,
+          minHeight: panelHeightClosed,
+          maxHeight: panelHeightOpened,
+          parallaxEnabled: true,
+          parallaxOffset: 0.2,
+          backdropEnabled: true,
+          backdropColor: Colors.black,
+          backdropOpacity: 0.5,
+          //color: Colors.green,
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(18)),
+          panelBuilder: (controller) => PanelWidget(
+            controller: controller,
+            panelController: panelController,
+            match: _selectedMatch,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(left: 14, top: 32, right: 14),
+            child: Column(
+              children: [
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: competitions
+                        .map(
+                          (e) => InkWell(
+                              onTap: () => setState(() {
+                                    _selectedCompetition = e.name;
+                                  }),
+                              child: CompetitionWidget(
+                                  image: e.images,
+                                  name: e.name,
+                                  isSelected: e.name == _selectedCompetition)),
+                        )
+                        .toList(),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 24,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: matchCards.map(
+                        (e) {
+                          return InkWell(
+                              onTap: () => setState(() {
+                                    _selectedMatch = e.match;
+                                    _slideupPanelUp = !_slideupPanelUp;
+                                  }),
+                              child: MatchCard(
+                                  match: e.match,
+                                  isSelected: e.match == _selectedMatch));
+                        },
+                      ).toList(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class PanelWidget extends StatelessWidget {
+  final ScrollController controller;
+  final PanelController panelController;
+  final Match match;
+  const PanelWidget(
+      {Key? key,
+      required this.controller,
+      required this.panelController,
+      required this.match})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      controller: controller,
+      children: [
+        const SizedBox(height: 12),
+        dragHandleBuilder(),
+        const SizedBox(height: 18),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        MatchCard(match: match, isSelected: true),
+        const SizedBox(height: 34)
+      ],
+    );
+  }
+
+  Widget dragHandleBuilder() {
+    return GestureDetector(
+      child: Center(
+        child: Container(
+          width: 30,
+          height: 5,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+      onTap: togglePanel,
+    );
+  }
+
+  void togglePanel() {
+    panelController.isPanelOpen
+        ? panelController.close()
+        : panelController.open();
   }
 }
