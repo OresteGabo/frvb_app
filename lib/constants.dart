48e24248f76f9032a3d7ed912a34ad7977503034
oreste <muhirwa.g.oreste@gmail.com>
2022-09-04 09:21:36 +0200

    constant variables were turned into static variables, for better usage

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-const backgroundColor = Color.fromARGB(235, 246, 246, 246);
+class appVars {
+  static Color backgroundColor = Color.fromARGB(235, 246, 246, 246);
 //Color backgroundColor = Colors.grey[300];
 //const backgroundColor = Colors.grey[300];
-const pinkColor = Color(0xffff0e80);
-const selectedColor = Color(0xffe5a457);
-const iconColors = Colors.grey;
-const selectedBorderColor = Colors.orange;
+  static Color pinkColor = Color(0xffff0e80);
+  static Color selectedColor = Color(0xffe5a457);
+  static Color iconColors = Colors.grey;
+  static Color selectedBorderColor = Colors.orange;
+  static bool darkMode = false;
+}
