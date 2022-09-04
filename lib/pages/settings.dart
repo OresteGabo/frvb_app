bc4c55f1345de0eaadaa4a6316b85b896177717b
oreste <muhirwa.g.oreste@gmail.com>
2022-09-04 07:01:57 +0200

    Making a switch interactive, but still not useful in the app

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -7,7 +8,12 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Settings"),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text(
+          "Settings",
+          style: Theme.of(context).textTheme.headline4,
+        ),
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back,
@@ -16,50 +22,142 @@ class SettingsPage extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: ListView(
+      body: Column(
         children: [
           Container(
             height: 40,
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
+              boxShadow: [
+                BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  blurStyle: BlurStyle.outer,
+                  color: Colors.grey.shade400,
+                ),
+              ],
+              //border: Border.all(color: Colors.white),
               borderRadius: BorderRadius.circular(15),
             ),
             margin: const EdgeInsets.all(24),
-            child: const TextField(),
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '    Search',
+              ),
+            ),
           ),
           const SizedBox(height: 12),
-          settingElement("Account", Icons.account_box),
-          const SizedBox(height: 6),
-          const Expanded(child: Divider(thickness: 1.0)),
-          const SizedBox(height: 6),
-          settingElement("Notifications", Icons.notification_add),
-          const SizedBox(height: 6),
-          const Expanded(child: Divider(thickness: 1.0)),
-          const SizedBox(height: 6),
-          settingElement("Appearance", Icons.panorama_fish_eye),
-          const SizedBox(height: 6),
-          const Expanded(child: Divider(thickness: 1.0)),
-          const SizedBox(height: 6),
-          settingElement("Privacy & security", Icons.lock),
-          const SizedBox(height: 6),
-          const Expanded(child: Divider(thickness: 1.0)),
-          const SizedBox(height: 6),
-          settingElement("About", Icons.question_mark_outlined),
-          const SizedBox(height: 6),
-          const Expanded(child: Divider(thickness: 1.0)),
-          const SizedBox(height: 6),
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  blurStyle: BlurStyle.outer,
+                  color: Colors.grey.shade400,
+                ),
+              ],
+              color: Colors.white,
+              //border: Border.all(color: Colors.grey),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 12),
+                settingElement("Account", Icons.account_box),
+                const SizedBox(height: 6),
+                settingElementsDivider(),
+                const SizedBox(height: 6),
+                settingElement("Notifications", Icons.notification_add),
+                const SizedBox(height: 6),
+                settingElementsDivider(),
+                const SizedBox(height: 6),
+                settingElement("Dark mode", Icons.dark_mode_rounded,
+                    isSwitchButton: true),
+                const SizedBox(height: 6),
+                settingElementsDivider(),
+                const SizedBox(height: 6),
+                settingElement("Privacy & security", Icons.lock),
+                const SizedBox(height: 6),
+                settingElementsDivider(),
+                const SizedBox(height: 6),
+                settingElement("About", Icons.question_mark_outlined),
+                const SizedBox(height: 12),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-Widget settingElement(String label, IconData icon,
-    {bool isSelected = false, bool isPremium = false}) {
-  return Row(children: [
-    const SizedBox(width: 24),
-    Icon(icon),
-    const SizedBox(width: 12),
-    Text(label),
-  ]);
+Widget settingElementsDivider() {
+  return Container(
+      margin: const EdgeInsets.only(left: 60),
+      child: const Expanded(child: Divider(thickness: 1.0)));
+}
+
+Widget settingElement(
+  String label,
+  IconData icon, {
+  IconData actionButton = Icons.arrow_forward_ios_rounded,
+  bool isSelected = false,
+  bool isPremium = false,
+  bool isSwitchButton = false,
+}) {
+  List<bool> _values = [true, false, true, false, false];
+  const int count = 4;
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(children: [
+        const SizedBox(width: 24),
+        Icon(
+          icon,
+          color: Colors.grey.shade600,
+        ),
+        const SizedBox(width: 12),
+        Text(label,
+            style: TextStyle(
+              color: Colors.grey.shade600,
+            )),
+      ]),
+      Row(
+        children: [
+          isSwitchButton
+              ? const SwitchButton()
+              : Icon(
+                  actionButton,
+                  color: Colors.grey.shade600,
+                ),
+          const SizedBox(
+            width: 12,
+          )
+        ],
+      ),
+    ],
+  );
+}
+
+class SwitchButton extends StatefulWidget {
+  const SwitchButton({Key? key}) : super(key: key);
+
+  @override
+  State<SwitchButton> createState() => _SwitchButtonState();
+}
+
+class _SwitchButtonState extends State<SwitchButton> {
+  bool _value = false;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSwitch(
+      value: _value,
+      activeColor: Colors.orangeAccent,
+      onChanged: (bool value) {
+        setState(() {
+          _value = value;
+        });
+      },
+    );
+  }
 }
