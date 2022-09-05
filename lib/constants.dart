78439502e06ee26fb5ea66b124bc018c5d2b1dbb
oreste <muhirwa.g.oreste@gmail.com>
2022-09-05 23:35:08 +0200

    creating better static variables for boxshadow, and their colors for both light and dark mode

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,5 +8,52 @@ class appVars {
   static Color selectedColor = Color(0xffe5a457);
   static Color iconColors = Colors.grey;
   static Color selectedBorderColor = Colors.orange;
-  static bool darkMode = false;
+  static bool isDark = false;
+  static BoxShadow darkmodeShadow = const BoxShadow();
+  static BoxShadow lightmodeShadow = const BoxShadow();
+
+  //appVars();
+
+  static Color darkmodeShadowColor = Colors.black45;
+  static Color lightmodeShadowColor = Colors.grey.shade400;
+  static BoxShadow boxShadow = isDark
+      ? BoxShadow(
+          spreadRadius: 2,
+          blurRadius: 10,
+          blurStyle: BlurStyle.outer,
+          color: appVars.darkmodeShadowColor,
+        )
+      : BoxShadow(
+          spreadRadius: 2,
+          blurRadius: 10,
+          blurStyle: BlurStyle.outer,
+          color: appVars.lightmodeShadowColor,
+        );
+
+  appVars() {
+    darkmodeShadow = BoxShadow(
+      spreadRadius: 2,
+      blurRadius: 10,
+      blurStyle: BlurStyle.outer,
+      color: darkmodeShadowColor,
+    );
+    lightmodeShadow = BoxShadow(
+      spreadRadius: 2,
+      blurRadius: 10,
+      blurStyle: BlurStyle.outer,
+      color: lightmodeShadowColor,
+    );
+  } //Color();
+
+  /*appVars (){
+    this.shadowColor = isDark ? Colors.black45 : Colors.grey.shade400;
+    this.darkmodeShadow = BoxShadow(
+      spreadRadius: 2,
+      blurRadius: 10,
+      blurStyle: BlurStyle.outer,
+      color: shadowColor,
+    );
+
+  }*/
+
 }
