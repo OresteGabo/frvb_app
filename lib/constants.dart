c6a9c89d86ace8dad02ae6c3742d9cd7216eb57c
oreste <muhirwa.g.oreste@gmail.com>
2022-09-06 00:10:03 +0200

    renaming of some vars, rearranging them with proper comments

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
 class appVars {
+  /// Global variables
   static Color backgroundColor = Color.fromARGB(235, 246, 246, 246);
-//Color backgroundColor = Colors.grey[300];
-//const backgroundColor = Colors.grey[300];
   static Color pinkColor = Color(0xffff0e80);
   static Color selectedColor = Color(0xffe5a457);
   static Color iconColors = Colors.grey;
   static Color selectedBorderColor = Colors.orange;
+
+  /// Push notification variables
   static bool isDark = false;
-  static bool liveGamesNotificationEnabled = false;
-  static bool gamesUpdateNotificationEnaled = false;
-  static bool favoriteAthleteUpdateNotificationEnaled = false;
+  static bool liveGamesNotificationsEnabled = false;
+  static bool gamesUpdateNotificationsEnaled = false;
+  static bool favoriteAthleteUpdateNotificationsEnabled = false;
+  static bool favoriteCompetitionUpdateNotificationsEnabled = false;
+  static bool favoriteTeamUpdateNotificationsEnabled = false;
 
-  static bool favoriteTeamUpdateNotificationEnaled = false;
-
+  /// Dark and light management variables
   static BoxShadow darkmodeShadow = const BoxShadow();
   static BoxShadow lightmodeShadow = const BoxShadow();
-
   static Color darkmodeShadowColor = Colors.black45;
   static Color lightmodeShadowColor = Colors.grey.shade400;
   static BoxShadow boxShadow = isDark ? darkmodeShadow : lightmodeShadow;
