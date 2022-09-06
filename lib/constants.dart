ad18c18de65dc8359b791b59f66ac491db66c805
oreste <muhirwa.g.oreste@gmail.com>
2022-09-06 03:11:25 +0200

    Colors added for text for both themes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
-class appVars {
+class AppVars {
   /// Global variables
-  static Color backgroundColor = Color.fromARGB(235, 246, 246, 246);
-  static Color pinkColor = Color(0xffff0e80);
-  static Color selectedColor = Color(0xffe5a457);
-  static Color iconColors = Colors.grey;
-  static Color selectedBorderColor = Colors.orange;
+  static const Color backgroundColor = Color.fromARGB(235, 246, 246, 246);
+  static const Color pinkColor = Color(0xffff0e80);
+  static const Color selectedColor = Color(0xffe5a457);
+  static const Color iconColors = Colors.grey;
+  static const Color selectedBorderColor = Colors.orange;
+  static const Color lightThemeTextColor = Colors.black54;
+  static const Color darkThemeTextColor = Colors.white70;
 
-  /// Push notification variables
+  /// Push notification variables  (controlled by Switch buttons)
   static bool isDark = false;
   static bool liveGamesNotificationsEnabled = false;
   static bool gamesUpdateNotificationsEnaled = false;
@@ -17,24 +19,19 @@ class appVars {
   static bool favoriteTeamUpdateNotificationsEnabled = false;
 
   /// Dark and light management variables
-  static BoxShadow darkmodeShadow = const BoxShadow();
-  static BoxShadow lightmodeShadow = const BoxShadow();
-  static Color darkmodeShadowColor = Colors.black45;
+  static const BoxShadow darkmodeShadow = BoxShadow(
+    spreadRadius: 1,
+    blurRadius: 10,
+    blurStyle: BlurStyle.outer,
+    color: darkmodeShadowColor,
+  );
+  static final BoxShadow lightmodeShadow = BoxShadow(
+    spreadRadius: 1,
+    blurRadius: 10,
+    blurStyle: BlurStyle.outer,
+    color: lightmodeShadowColor,
+  );
+  static const Color darkmodeShadowColor = Colors.black45;
   static Color lightmodeShadowColor = Colors.grey.shade400;
   static BoxShadow boxShadow = isDark ? darkmodeShadow : lightmodeShadow;
-
-  appVars() {
-    darkmodeShadow = BoxShadow(
-      spreadRadius: 2,
-      blurRadius: 10,
-      blurStyle: BlurStyle.outer,
-      color: darkmodeShadowColor,
-    );
-    lightmodeShadow = BoxShadow(
-      spreadRadius: 2,
-      blurRadius: 10,
-      blurStyle: BlurStyle.outer,
-      color: lightmodeShadowColor,
-    );
-  }
 }
