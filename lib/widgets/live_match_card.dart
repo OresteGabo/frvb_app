bb189eaf037494ff40e9f4aac551e9aa11a42a42
oreste <muhirwa.g.oreste@gmail.com>
2022-09-07 03:15:18 +0200

    More informations were added in the card, including the number of sets played, live icon and how the last sets was won

--- lib/widgets/live_match_card.dart
+++ lib/widgets/live_match_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frvb/widgets/live_match_column.dart';
 
@@ -26,7 +27,7 @@ class LiveMatchCard extends StatelessWidget {
             height: 5,
           ),
           Text(
-            "Week 10",
+            'SET ${match.awaySetsWon + match.homeSetsWon}',
             style: Theme.of(context)
                 .textTheme
                 .headline6!
@@ -44,6 +45,26 @@ class LiveMatchCard extends StatelessWidget {
                 children: [
                   Row(
                     children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                            //color: Colors.green,
+                            ),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text(
+                            '${match.homeSetsWon}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(
+                                  //color: Colors.orangeAccent,
+                                  color:
+                                      const Color.fromRGBO(199, 100, 65, 0.7),
+                                ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 6),
                       Text(
                         match.homeGoals.toString(),
                         style: Theme.of(context)
@@ -71,6 +92,20 @@ class LiveMatchCard extends StatelessWidget {
                             .headline3!
                             .copyWith(color: Colors.white),
                       ),
+                      const SizedBox(width: 6),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          '${match.awaySetsWon}',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(
+                                //color: Colors.orangeAccent,
+                                color: const Color.fromRGBO(199, 100, 65, 0.7),
+                              ),
+                        ),
+                      ),
                     ],
                   ),
                   Container(
@@ -78,13 +113,33 @@ class LiveMatchCard extends StatelessWidget {
                         const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xff493f5d),
+                        color: const Color(0x7e493f5d),
                         border: Border.all(color: Colors.orange, width: 2)),
-                    child: const Text(
-                      "83'",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    child: Text(
+                      '${Match.random(10, 100)}\'',
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
                     ),
-                  )
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(200, 65, 54, 0.5),
+                      borderRadius: BorderRadius.circular(34),
+                      border: Border.all(
+                        color: Colors.red,
+                      ),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        color: Colors.red,
+                        //Icons.radio_button_unchecked_rounded,
+                        CupertinoIcons.antenna_radiowaves_left_right,
+                      ),
+                    ),
+                  ),
                 ],
               ),
               LiveMatchColumn(team: match.awayTeam, isHome: false),
