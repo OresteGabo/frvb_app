5b5d625ce04cbbdede0e2d77268c72f253f07e17
oreste <muhirwa.g.oreste@gmail.com>
2022-09-08 09:34:09 +0200

    Some designs added, esp colors of different elements in  darkmode

--- lib/model/theme_provider.dart
+++ lib/model/theme_provider.dart
@@ -3,12 +3,25 @@ import 'package:frvb/constants.dart';
 
 class MyThemes {
   static final darkTheme = ThemeData(
+    cardColor: AppVars.darkThemeCardBackgroundColor,
     scaffoldBackgroundColor: AppVars.darkThemeBackgroundColor,
     colorScheme: const ColorScheme.dark(),
+    inputDecorationTheme: const InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        borderSide: BorderSide(color: AppVars.darkThemeBackgroundColor),
+      ),
+      hintStyle: TextStyle(
+        color: Color.fromRGBO(180, 180, 185, 1),
+      ),
+      filled: true, //<-- SEE HERE
+      fillColor: Color.fromRGBO(41, 41, 43, 1), //<-- SEE HERE
+    ),
   );
   static final lightTheme = ThemeData(
     scaffoldBackgroundColor: AppVars.lightThemeBackgroundColor,
     colorScheme: const ColorScheme.light(),
+    cardColor: Colors.white,
     inputDecorationTheme: const InputDecorationTheme(
       border: OutlineInputBorder(
         borderRadius: BorderRadius.all(Radius.circular(10.0)),
