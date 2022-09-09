192a207539e4eb9bf2b055746d34e156503cd418
oreste <muhirwa.g.oreste@gmail.com>
2022-09-09 04:19:14 +0200

    This will represent a single bookmark, that will be represented in a bookmark page

---
+++ lib/widgets/bookmark.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BookMark extends StatefulWidget {
+  const BookMark({Key? key}) : super(key: key);
+
+  @override
+  State<BookMark> createState() => _BookMarkState();
+}
+
+class _BookMarkState extends State<BookMark> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class GameBookMark extends BookMark {}
+
+class CompetitionBookMark extends BookMark {}
