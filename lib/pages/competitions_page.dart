4a614feba67b475bf2d472668a7444ce1ba6a6cf
oreste <muhirwa.g.oreste@gmail.com>
2022-09-11 10:14:49 +0200

    A list of competitions was added to the page to thest how each card react to onTap, and to favorite icon

--- lib/pages/competitions_page.dart
+++ lib/pages/competitions_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:frvb/model/competition.dart';
 
-class CompetitionsPage extends StatelessWidget {
-  //final List<Competition> competitions;
-  const CompetitionsPage({
-    Key? key,
-    //required this.competitions,
-  }) : super(key: key);
+import '../widgets/competition_card.dart';
+
+class CompetitionsPage extends StatefulWidget {
+  const CompetitionsPage({Key? key}) : super(key: key);
 
   @override
+  State<CompetitionsPage> createState() => _CompetitionPageState();
+}
+
+class _CompetitionPageState extends State<CompetitionsPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    var width = MediaQuery.of(context).size.width;
+    var _selectedCompetition = competitions[1];
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: competitions.map(
+            (e) {
+              return InkWell(
+                onTap: () => setState(() {
+                  _selectedCompetition = e;
+                }),
+                child: CompetitionCard(
+                  width: width,
+                  isSelected: e == _selectedCompetition,
+                  competition: _selectedCompetition,
+                ),
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
   }
 }
