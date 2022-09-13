f7c6091ccb02735f2c1215e991709760834d09f8
oreste <muhirwa.g.oreste@gmail.com>
2022-09-13 22:50:36 +0200

    constructor xas changed to oblige the names while creating objects

--- lib/model/team.dart
+++ lib/model/team.dart
@@ -1,20 +1,18 @@
 class Team {
-  final String _logo;
-  final String _name;
+  final String logo;
+  final String name;
   late final fullName;
 
-  Team(this._logo, this._name, {this.fullName = "NO FULL NAME"});
-
-  String get name => _name;
-  String get logo => _logo;
+  Team(
+      {required this.logo, required this.name, this.fullName = "NO FULL NAME"});
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is Team && runtimeType == other.runtimeType && _name == other._name;
+      other is Team && runtimeType == other.runtimeType && name == other.name;
 
   @override
-  int get hashCode => _name.hashCode;
+  int get hashCode => name.hashCode;
 
   //String get fullName => _fullName;
 
@@ -28,29 +26,35 @@ class Team {
   }
 }
 
-final Team gisagara = Team("assets/clubs/gisagara.webp", "Gisagara");
-final Team aprvc = Team("assets/clubs/aprvc.png", "APR VC");
+final Team gisagara =
+    Team(logo: "assets/clubs/gisagara.webp", name: "Gisagara");
+final Team aprvc = Team(logo: "assets/clubs/aprvc.png", name: "APR VC");
 final Team christianUniv =
-    Team("assets/clubs/christianuniversity.webp", "ChrisUnv");
-final Team bugiri = Team("assets/clubs/bugirivbc", "BUGIRI");
-final Team sainJoseph = Team("assets/clubs/gs_saint-joseph", "st Joseph");
-final Team gsjinaa = Team("assets/clubs/gsjinja.png", "GS JINJA");
-final Team gsob = Team("assets/clubs/gsob.png", "GSOB");
-final Team iprcsouth = Team("assets/clubs/iprcsouth.png", "IPRC ST");
-final Team kavc = Team("assets/clubs/kavc.webp", "KAVC");
-final Team kirehe = Team("assets/clubs/kirehe.png", "KIREHE");
-final Team kvc = Team("assets/clubs/kvc.png", "KVC");
-final Team muzinga = Team("assets/clubs/muzinga.webp", "MUZINGA");
-final Team ndejje = Team("assets/clubs/ndejje.webp", "NDEJJE");
-final Team nemostars = Team("assets/clubs/nemostars.webp", "NEMOSTARS");
-final Team rayon = Team("assets/clubs/rayon.webp", "RAYON VC");
-final Team reg = Team("assets/clubs/reg.webp", "REG VC");
-final Team rra = Team("assets/clubs/rra.png", "RRA VC");
-final Team rukinzo = Team("assets/clubs/rukinzo.png", "RUKINZO");
-final Team rwandaarmy = Team("assets/clubs/rwandaarmy.png", "RW ARMY VC");
-final Team unik = Team("assets/clubs/unik.webp", "UNIK VC");
-final Team ur = Team("assets/clubs/ur.png", "UR VC");
-final Team utb = Team("assets/clubs/utb_cropped_logo.png", "UTB VC");
+    Team(logo: "assets/clubs/christianuniversity.webp", name: "ChrisUnv");
+final Team bugiri = Team(logo: "assets/clubs/bugirivbc", name: "BUGIRI");
+final Team sainJoseph =
+    Team(logo: "assets/clubs/gs_saint-joseph", name: "st Joseph");
+final Team gsjinaa = Team(logo: "assets/clubs/gsjinja.png", name: "GS JINJA");
+final Team gsob = Team(name: "assets/clubs/gsob.png", logo: "GSOB");
+final Team iprcsouth =
+    Team(logo: "assets/clubs/iprcsouth.png", name: "IPRC ST");
+final Team kavc = Team(logo: "assets/clubs/kavc.webp", name: "KAVC");
+final Team kirehe = Team(logo: "assets/clubs/kirehe.png", name: "KIREHE");
+final Team kvc = Team(logo: "assets/clubs/kvc.png", name: "KVC");
+final Team muzinga = Team(logo: "assets/clubs/muzinga.webp", name: "MUZINGA");
+final Team ndejje = Team(logo: "assets/clubs/ndejje.webp", name: "NDEJJE");
+final Team nemostars =
+    Team(logo: "assets/clubs/nemostars.webp", name: "NEMOSTARS");
+final Team rayon = Team(logo: "assets/clubs/rayon.webp", name: "RAYON VC");
+final Team reg = Team(logo: "assets/clubs/reg.webp", name: "REG VC");
+final Team rra = Team(logo: "assets/clubs/rra.png", name: "RRA VC");
+final Team rukinzo = Team(logo: "assets/clubs/rukinzo.png", name: "RUKINZO");
+final Team rwandaarmy =
+    Team(logo: "assets/clubs/rwandaarmy.png", name: "RW ARMY VC");
+final Team unik = Team(logo: "assets/clubs/unik.webp", name: "UNIK VC");
+final Team ur = Team(logo: "assets/clubs/ur.png", name: "UR VC");
+final Team utb =
+    Team(logo: "assets/clubs/utb_cropped_logo.png", name: "UTB VC");
 
 List<Team> favoriteTeams = [
   gisagara,
