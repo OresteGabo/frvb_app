0a9f62e72a82bcb4fbd029f51b6692a821f807e1
oreste <muhirwa.g.oreste@gmail.com>
2022-09-24 01:34:04 +0200

    article class that will be used in newspage

---
+++ lib/model/article.dart
@@ -0,0 +1,133 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final commentsCount;
+
+  const Article(
+      {this.text = "",
+      this.domain = "",
+      this.by = "",
+      this.age = "",
+      this.score = 0,
+      this.commentsCount = 0});
+}
+
+final article = [
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  const Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 45,
+  ),
+  const Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+];
