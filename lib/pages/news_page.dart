f7e3172c1b15249452983fc9d8ba65e2c67a7147
oreste <muhirwa.g.oreste@gmail.com>
2022-09-24 16:53:27 +0200

    Updated article, with report and help option

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -22,35 +22,124 @@ class _NewsPageState extends State<NewsPage> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         title: Text(widget.title, style: Theme.of(context).textTheme.headline4),
       ),
-      body: Center(
-        child: ListView(
-          children: _articles.map((article) {
-            return _buildItem(article);
-          }).toList(),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(const Duration(seconds: 1));
+          setState(() {
+            //_articles.removeAt(0);
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(right: 18.0, left: 18.0),
+          child: Center(
+            child: ListView(
+              children: _articles.map((article) {
+                return _buildItem(article);
+              }).toList(),
+            ),
+          ),
         ),
       ),
     );
   }
 
   Widget _buildItem(Article article) {
+    final Uri _url = Uri.parse(article.domain);
+    bool _isSelected = true;
+    Color notSelectedColor = Colors.black;
+    Color selectedColor = Colors.orangeAccent;
     return ExpansionTile(
+      key: Key(article.domain),
       title: Text(article.text),
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Author: ${article.by}'),
+            Row(
+              children: [
+                //this button will be used to report the article
+                PopupMenuButton(
+                  icon: const Icon(Icons.more_vert),
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                    const PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(Icons.report),
+                        title: Text('Report'),
+                        subtitle:
+                            Text('Reported an articles will be inspected'),
+                      ),
+                    ),
+                    const PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(Icons.help),
+                        title: Text('Help'),
+                        subtitle: Text("more information about this article"),
+                      ),
+                    ),
+                    /*
+                    const PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(Icons.article),
+                        title: Text('Item 3'),
+                      ),
+                    ),
+                    const PopupMenuDivider(),
+                    const PopupMenuItem(child: Text('Item A')),
+                    const PopupMenuItem(child: Text('Item B')),*/
+                  ],
+                ),
+                IconButton(
+                    color: _isSelected ? selectedColor : notSelectedColor,
+                    padding: const EdgeInsets.only(right: 0),
+                    onPressed: () {
+                      setState(() {
+                        if (_isSelected == true) {
+                          _isSelected = false;
+                          article.likesCount--;
+                        } else {
+                          _isSelected = true;
+                          article.likesCount++;
+                        }
+                      });
+                    },
+                    icon: const Icon(Icons.favorite)),
+                Text('${article.likesCount} likes'),
+              ],
+            ),
+            //Text('Author: ${article.by}'),
             Text('${article.commentsCount} comments'),
-            Container(
+            /*Container(
               color: Colors.green,
               child: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _launchUrl(_url);
+                  });
+                },
                 icon: const Icon(Icons.launch),
               ),
-            )
+            )*/
+            Container(
+              color: Colors.green,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Read article',
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
           ],
         ),
       ],
     );
+  }
+
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
   }
 }
