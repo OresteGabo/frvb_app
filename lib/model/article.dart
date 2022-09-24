11401b36fa93011f6883b344957e23aad1689652
oreste <muhirwa.g.oreste@gmail.com>
2022-09-24 16:54:31 +0200

    likes var added on an article

--- lib/model/article.dart
+++ lib/model/article.dart
@@ -5,18 +5,39 @@ class Article {
   final String age;
   final int score;
   final int commentsCount;
+  int likesCount;
 
-  const Article(
+  Article(
       {this.text = "",
       this.domain = "",
       this.by = "",
       this.age = "",
       this.score = 0,
-      this.commentsCount = 0});
+      this.commentsCount = 0,
+      this.likesCount = 0});
 }
 
-const articles = [
+var articles = [
   Article(
+    text:
+        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
+    by: "iradukunda olivier",
+    age: "2 hours",
+    score: 25,
+    commentsCount: 47,
+  ),
+  Article(
+    text: "Yves Mutabazi agiye kwimukira muri Amerika",
+    domain:
+        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
+    by: "Nsengiyumva Emmy",
+    age: "3 days",
+    score: 34,
+    commentsCount: 45,
+  ),
+  /*Article(
     text:
         "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
     domain:
@@ -338,24 +359,5 @@ const articles = [
     age: "3 days",
     score: 34,
     commentsCount: 45,
-  ),
-  Article(
-    text:
-        "Amakipe azaserukira u Rwanda akomeje kwitegura Shampiyona y’Isi muri Sitting Volleyball ",
-    domain:
-        "https://igihe.com/imikino/volleyball/article/amakipe-azaserukira-u-rwanda-akomeje-kwitegura-shampiyona-y-isi-muri-sitting",
-    by: "iradukunda olivier",
-    age: "2 hours",
-    score: 25,
-    commentsCount: 45,
-  ),
-  Article(
-    text: "Yves Mutabazi agiye kwimukira muri Amerika",
-    domain:
-        "https://igihe.com/imikino/volleyball/article/yves-mutabazi-agiye-kwimukira-muri-leta-zunze-ubumwe-za-amerika",
-    by: "Nsengiyumva Emmy",
-    age: "3 days",
-    score: 34,
-    commentsCount: 45,
-  ),
+  ),*/
 ];
