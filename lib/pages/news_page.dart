19ddc100a6433a7dabb3f050519147831b2eca86
oreste <muhirwa.g.oreste@gmail.com>
2022-09-24 17:51:49 +0200

    tabs added,to sort from top stories,saved stories, and all stories, (All stories will be sorted according to the date of publishing,but top stories will be sorted according to the number of likes and other factors that will be added),  and background image was added as well

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,6 +1,9 @@
+import 'package:frvb/constants.dart';
 import 'package:frvb/model/article.dart';
 import 'package:flutter/material.dart';
+import 'package:frvb/model/theme_provider.dart';
 import 'package:frvb/widgets/app_drawer.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NewsPage extends StatefulWidget {
@@ -15,29 +18,65 @@ class _NewsPageState extends State<NewsPage> {
   final List<Article> _articles = articles;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: AppDrawer(),
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: Text(widget.title, style: Theme.of(context).textTheme.headline4),
-      ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          await Future.delayed(const Duration(seconds: 1));
-          setState(() {
-            //_articles.removeAt(0);
-          });
-        },
-        child: Padding(
-          padding: const EdgeInsets.only(right: 18.0, left: 18.0),
-          child: Center(
-            child: ListView(
-              children: _articles.map((article) {
-                return _buildItem(article);
-              }).toList(),
-            ),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        drawer: AppDrawer(),
+        appBar: AppBar(
+          flexibleSpace: Image(
+            image: AppVars.palette
+                .getDecorationImage(themeProvider.isDarkMode)
+                .image,
+            fit: BoxFit.cover,
           ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          bottom: const TabBar(
+            labelColor: AppVars.selectedColor, //<-- selected text color
+            unselectedLabelColor: Colors.grey,
+
+            indicatorColor: AppVars.selectedColor,
+            tabs: [
+              Tab(
+                text: 'Top stories',
+              ),
+              Tab(
+                text: 'Saved Stories',
+              ),
+              Tab(
+                text: 'All Stories',
+              ),
+            ],
+          ),
+          // backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          //title:
+          //  Text(widget.title, style: Theme.of(context).textTheme.headline4),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await Future.delayed(const Duration(seconds: 1));
+                  setState(() {
+                    //_articles.removeAt(0);
+                  });
+                },
+                child: Center(
+                  child: ListView(
+                    children: _articles.map((article) {
+                      return _buildItem(article);
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+            const Center(child: Text('Saved stories will be here')),
+            const Center(
+                child: Text(
+                    'All storied, sorted by time of articles, and the number of likes')),
+          ],
         ),
       ),
     );
@@ -48,92 +87,97 @@ class _NewsPageState extends State<NewsPage> {
     bool _isSelected = true;
     Color notSelectedColor = Colors.black;
     Color selectedColor = Colors.orangeAccent;
-    return ExpansionTile(
-      key: Key(article.domain),
-      title: Text(article.text),
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                //this button will be used to report the article
-                PopupMenuButton(
-                  icon: const Icon(Icons.more_vert),
-                  itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                    const PopupMenuItem(
-                      child: ListTile(
-                        leading: Icon(Icons.report),
-                        title: Text('Report'),
-                        subtitle:
-                            Text('Reported an articles will be inspected'),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ExpansionTile(
+        key: Key(article.domain),
+        title: Text(article.text),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  //this button will be used to report the article
+                  PopupMenuButton(
+                    icon: const Icon(Icons.more_vert),
+                    itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                      const PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(Icons.report),
+                          title: Text('Report'),
+                          subtitle:
+                              Text('Reported an articles will be inspected'),
+                        ),
                       ),
-                    ),
-                    const PopupMenuItem(
-                      child: ListTile(
-                        leading: Icon(Icons.help),
-                        title: Text('Help'),
-                        subtitle: Text("more information about this article"),
+                      const PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(Icons.help),
+                          title: Text('Help'),
+                          subtitle: Text("more information about this article"),
+                        ),
                       ),
-                    ),
-                    /*
-                    const PopupMenuItem(
-                      child: ListTile(
-                        leading: Icon(Icons.article),
-                        title: Text('Item 3'),
+                      /*
+                      const PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(Icons.article),
+                          title: Text('Item 3'),
+                        ),
                       ),
-                    ),
-                    const PopupMenuDivider(),
-                    const PopupMenuItem(child: Text('Item A')),
-                    const PopupMenuItem(child: Text('Item B')),*/
-                  ],
-                ),
-                IconButton(
-                    color: _isSelected ? selectedColor : notSelectedColor,
-                    padding: const EdgeInsets.only(right: 0),
-                    onPressed: () {
-                      setState(() {
-                        if (_isSelected == true) {
-                          _isSelected = false;
-                          article.likesCount--;
-                        } else {
-                          _isSelected = true;
-                          article.likesCount++;
-                        }
-                      });
-                    },
-                    icon: const Icon(Icons.favorite)),
-                Text('${article.likesCount} likes'),
-              ],
-            ),
-            //Text('Author: ${article.by}'),
-            Text('${article.commentsCount} comments'),
-            /*Container(
-              color: Colors.green,
-              child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _launchUrl(_url);
-                  });
-                },
-                icon: const Icon(Icons.launch),
-              ),
-            )*/
-            Container(
-              color: Colors.green,
-              child: InkWell(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Read article',
+                      const PopupMenuDivider(),
+                      const PopupMenuItem(child: Text('Item A')),
+                      const PopupMenuItem(child: Text('Item B')),*/
+                    ],
                   ),
-                ),
-                onTap: () {},
+                  IconButton(
+                      color: _isSelected ? selectedColor : notSelectedColor,
+                      padding: const EdgeInsets.only(right: 0),
+                      onPressed: () {
+                        setState(() {
+                          if (_isSelected == true) {
+                            _isSelected = false;
+                            article.likesCount--;
+                          } else {
+                            _isSelected = true;
+                            article.likesCount++;
+                          }
+                        });
+                      },
+                      icon: const Icon(Icons.favorite)),
+                  Text('${article.likesCount} likes'),
+                ],
               ),
-            ),
-          ],
-        ),
-      ],
+              //Text('Author: ${article.by}'),
+              Text('${article.commentsCount} comments'),
+              /*Container(
+                color: Colors.green,
+                child: IconButton(
+                  onPressed: () {
+                    
+                  },
+                  icon: const Icon(Icons.launch),
+                ),
+              )*/
+              Container(
+                color: Colors.green,
+                child: InkWell(
+                  child: const Padding(
+                    padding: EdgeInsets.all(6.0),
+                    child: Text(
+                      'Read article',
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _launchUrl(_url);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
